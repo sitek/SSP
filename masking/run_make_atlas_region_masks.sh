84d966792b8badfe4c00669351a1346c8dac6c59
Kevin Sitek <sitek.kevin@gmail.com>
2026-08-05 16:39:01 -0400

    Rerunning

--- masking/run_make_atlas_region_masks.sh
+++ masking/run_make_atlas_region_masks.sh
@@ -8,12 +8,12 @@ for subpath in /bgfs/bchandrasekaran/krs228/data/SSP/data_bids/sub*/; do
   subid=$(basename $subpath)
   echo $subid
   sub_label=${subid#"sub-"}
-#for sub_label in 'SSP013' 'SSP040' 'SSP089'  'SSP091' 'SSP093' 'SSP095'  'SSP097' 'SSP099' 'SSP100' 'SSP103'; do
+#for sub_label in 'SSP013' 'SSP018' 'SSP032'; do
 #  echo $sub_label
   python make_atlas_region_masks.py --sub=$sub_label \
     --space=MNI152NLin2009cAsym \
     --fwhm=0.00 \
-    --atlas_label=carpet_motor \
+    --atlas_label=carpet_dseg \
     --bidsroot=/bgfs/bchandrasekaran/krs228/data/SSP/data_bids/ \
     --fmriprep_dir=/bgfs/bchandrasekaran/krs228/data/SSP/data_bids/derivatives/fmriprep-23.2.1/
 done
